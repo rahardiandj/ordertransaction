26e0480dbf0e12d5ba0cd1f4a1d45f9e4eba448e
rahardiandj <rahardiandj@gmail.com>
2017-09-27 10:18:09 +0700

    Initial project for OrderTransaction Project

---
+++ customer/src/main/java/org/ordertransaction/customer/App.java
@@ -0,0 +1,13 @@
+package org.ordertransaction.customer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
